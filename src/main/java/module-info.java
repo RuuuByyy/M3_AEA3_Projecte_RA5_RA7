1ef9f7a111a8dd0da5205ece172fa2969433b51f
RUBEN <rubencoaviles@gmail.com>
2026-03-11 18:32:29 +0100

    primer commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module perez.cordero.projecte_ra5_ra7 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens perez.cordero.projecte_ra5_ra7 to javafx.fxml;
+    exports perez.cordero.projecte_ra5_ra7;
+}
